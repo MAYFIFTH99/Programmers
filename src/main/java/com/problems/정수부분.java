1437dd33be9f79c1a70b0614ca79c32cb93e6abc
성민석 <alstjr971@naver.com>
2026-07-21 15:07:40 +0900

    double을 int로 type casting

---
+++ src/main/java/com/problems/정수부분.java
@@ -0,0 +1,13 @@
+package com.problems;
+
+public class 정수부분 {
+
+    public static void main(String[] args) {
+        double flo = 1.234;
+        // flo의 정수부분만 출력하기 int
+        int answer = (int)flo;
+        System.out.println(answer);
+
+    }
+
+}
