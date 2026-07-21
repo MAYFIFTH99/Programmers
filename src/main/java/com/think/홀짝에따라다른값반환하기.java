888dbef38ed8a3dfdb41b0a9685e9484fb7f16f3
성민석 <alstjr971@naver.com>
2026-07-21 15:04:34 +0900

    if/else와 삼항 연산자를 이용해 간결하게 작성하는 법 연습

---
+++ src/main/java/com/think/홀짝에따라다른값반환하기.java
@@ -0,0 +1,59 @@
+package com.think;
+
+/**
+ * 문제 설명
+ * 양의 정수 n이 매개변수로 주어질 때, n이 홀수라면 n 이하의 홀수인 모든 양의 정수의 합을 return 하고 n이 짝수라면 n 이하의 짝수인 모든 양의 정수의 제곱의 합을 return 하는 solution 함수를 작성해 주세요.
+ *
+ * 제한사항
+ * 1 ≤ n ≤ 100
+ * 입출력 예
+ * n	result
+ * 7	16
+ * 10	220
+ * 입출력 예 설명
+ * 입출력 예 #1
+ *
+ * 예제 1번의 n은 7로 홀수입니다. 7 이하의 모든 양의 홀수는 1, 3, 5, 7이고 이들의 합인 1 + 3 + 5 + 7 = 16을 return 합니다.
+ * 입출력 예 #2
+ *
+ * 예제 2번의 n은 10으로 짝수입니다. 10 이하의 모든 양의 짝수는 2, 4, 6, 8, 10이고 이들의 제곱의 합인 22 + 42 + 62 + 82 + 102 = 4 + 16 + 36 + 64 + 100 = 220을 return 합니다.
+ */
+public class 홀짝에따라다른값반환하기 {
+
+    public static void main(String[] args) {
+        int result = Solution.solution(10);
+        int result2 = Solution.solution2(10);
+        System.out.println(result);
+        System.out.println(result2);
+    }
+
+    static class Solution {
+        static int solution(int n) {
+            // n odd : n이하의 모든 홀수의 합
+            // n even : n이하의 모든 정수의 제곱들의 합
+
+            int odd = 0;
+            int even = 0;
+            if (n % 2 != 0) {
+                for (int i = n; i >= 1 ; i-=2) {
+                    odd += i;
+                }
+                return odd;
+            } else {
+                for (int i = n; i >= 1 ; i-=2) {
+                    even += i*i;
+                }
+                return even;
+            }
+        }
+
+        static int solution2(int n) {
+            int answer = 0;
+            for (int i = n; i >=0 ; i-=2) {
+                answer += (n % 2 == 0 ? i * i : i);
+            }
+            return answer;
+        }
+    }
+
+}
