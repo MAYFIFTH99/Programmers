517388622d763aeeafd0e3d67f2af489971e6ed9
성민석 <alstjr971@naver.com>
2026-07-21 13:49:51 +0900

    feat: String.join & StringBuilder
    - 단위/크기가 작을 때는 StringBuilder가 오히려 비효율적일 수 있음-> 고정 버퍼 크기를 생성하기 때문.
    - 근데 그 차이가 크지 않으니 대부분의 경우 Builder 를 쓰는게 메모리 성능 관점에서는 더 좋다.

---
+++ src/main/java/com/problems/문자리스트를문자열로변환하기.java
@@ -0,0 +1,37 @@
+package com.problems;
+
+/**
+ *문제 설명
+ * 문자들이 담겨있는 배열 arr가 주어집니다. arr의 원소들을 순서대로 이어 붙인 문자열을 return 하는 solution함수를 작성해 주세요.
+ *
+ * 제한사항
+ * 1 ≤ arr의 길이 ≤ 200
+ * arr의 원소는 전부 알파벳 소문자로 이루어진 길이가 1인 문자열입니다.
+ * 입출력 예
+ * arr	result
+ * ["a","b","c"]	"abc"
+ */
+public class 문자리스트를문자열로변환하기 {
+
+    public static void main(String[] args) {
+        String result = Solution.solution(new String[]{"a", "b", "c"});
+        System.out.println(result);
+        String result2 = Solution.solution2(new String[]{"a", "b", "c"});
+        System.out.println(result2);
+
+    }
+    static class Solution {
+        public static String solution(String[] arr) {
+            StringBuilder sb = new StringBuilder();
+
+            for (String s : arr) {
+                sb.append(s);
+            }
+            return sb.toString();
+        }
+
+        public static String solution2(String[] arr) {
+            return String.join("", arr);
+        }
+    }
+}
