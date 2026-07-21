d37dc04c6941b1632d965e4701fd7765db2a1cbc
성민석 <alstjr971@naver.com>
2026-07-21 15:45:35 +0900

    size = (size + n - 1) / n

--- src/main/java/com/think/n개간격의원소들.java
+++ src/main/java/com/think/n개간격의원소들.java
@@ -25,18 +25,39 @@ import java.util.Arrays;
 public class n개간격의원소들 {
 
     public static void main(String[] args) {
-        int[] result = Solution1.solution(new int[]{4, 2, 6, 1, 7, 6}, 2);
+        int[] result = Solution1.solution(new int[]{4, 2, 6, 1, 7, 6}, 4);
         System.out.println(Arrays.toString(result));
     }
 
     static class Solution1{
+        // 원소가 6개일 때 - 3개 필요 n/2
+        // 원소가 7개일 때 - 4개 필요 n/2 + 1
         public static int[] solution(int[] num_list, int n) {
-            int[] answer = new int[(num_list.length + n - 1) / n];
 
-            for (int i = 0, j = 0; i < num_list.length; i += n) {
-                answer[j++] = num_list[i];
+            // Think1 : 배열의 길이가 n으로 나누었을 때 나머지가 남는다면, 필요한 크기에 +1을 해준다.
+            int size = num_list.length % n != 0 ? num_list.length/n + 1 : num_list.length/n;
+
+            // Think2 : 위 식을 좀 풀어보면
+            int size2 = num_list.length / n;
+
+            if (num_list.length % n != 0) {
+                size2++;
             }
-            return answer;
+
+            //Think3 : 위 식을 다시 공식화 한다면
+            int size3 = (num_list.length + n - 1) / n;
+            // ★★★★ (전체 길이 + 간격 -1 ) / 간격 ★★★★
+
+
+            //----- 풀이
+            int[] arr = new int[size3];
+
+            for (int i = 0, j=0; i < num_list.length; i+=n) {
+                arr[j++] = num_list[i];
+            }
+
+            System.out.println(Arrays.toString(arr));
+            return arr;
         }
     }
 }
